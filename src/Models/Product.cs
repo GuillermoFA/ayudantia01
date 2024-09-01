d4360d1eee7aee88d4140002e9aff8fcf64db866
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-08-31 20:08:52 -0400

    feat: entityframework relationships

--- src/Models/Product.cs
+++ src/Models/Product.cs
@@ -8,8 +8,11 @@ namespace api.src.Models
     public class Product
     {
         // Atributos
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int Price { get; set; }
+        public int Id { get; set; } // 1 
+        public string Name { get; set; } = string.Empty; // arroz
+        public int Price { get; set; } // 1500
+
+        // Entityframework relationships
+        public List<User> Users { get; set; } = []; // juan, pedro, maria, jose...
     }
 }
--- src/Models/User.cs
+++ src/Models/User.cs
@@ -7,9 +7,18 @@ namespace api.src.Models
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Rut { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
+        public int Id { get; set; } // 1
+        public string Rut { get; set; } = string.Empty; // 2141241
+        public string Name { get; set; } = string.Empty; // Juan
         public string Email { get; set; } = string.Empty;
+
+
+        // Entityframework relationships
+        public List<Product> Products { get; set; } = []; // arroz, azucar, leche, pan...
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!; // admin, user, guest...
+
+
     }
 }
