02d4b06f29036c899a509d7a64d8351db7ed2652
GuillermoFA <guillermo.fuentes01@alumnos.ucn.cl>
2024-10-05 20:30:30 -0300

    feat: Cloudinary model

--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b10ea4d28ae48519644637b601f9c47a8014303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea648af41ecfaf4e93661694b8069a3cc30f7e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
